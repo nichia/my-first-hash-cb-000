1a40ba4adea69d92891876499fa611924ecd4695
Ni Chia <nichia@gmail.com>
2017-10-25 13:59:54 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,21 +1,24 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   member = {
-    "surname" => "Chia", 
-    "firtname" => "Ni", 
-    "birthdate" => 19710525, 
+    "surname" => "Chia",
+    "firtname" => "Ni",
+    "birthdate" => 19710525,
     "gender" => "f"}
   member["surname"]
   member["weight"] = 100
   member
 end
 
-
 def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  shipping_manifest = {
+    "whale bone cosets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 
 end
 
@@ -27,11 +30,8 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
+
   #your code here
   #remember to return the shipping_manifest hash
 
 end
-
-
-
